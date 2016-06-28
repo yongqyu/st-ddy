33ebfec5609c522ee5879f8e4a88751453f48778
yongqyu <yongqyu@gmail.com>
2016-06-28 20:16:17 +0900

    Update QUADTREE_yongqyu.cpp

--- QUADTREE/QUADTREE_yongqyu.cpp
+++ QUADTREE/QUADTREE_yongqyu.cpp
@@ -11,6 +11,15 @@
 
 using namespace std;
 
+//class QT
+//describe : quadtree node
+//attribute value - value what this node has
+//          sub - sub is 4 subtree. order is [0][0],[0][1],[1][0],[1][1]
+//                after process, print [1][0],[1][1],[0][0],[0][1]
+//
+//function initialize() - initialize 4 subtree by dynamic allocate and value by 0
+//         print_value() - print own value and sub's value
+//
 class QT{
 public:
     char value;
@@ -20,6 +29,8 @@ public:
     void print_value();
 };
 
+//function void QT::initialize()
+//describe : initialize 4 subtree by dynamic allocate and value by 0
 void QT::initialize()
 {
     QT **temp = new QT*[2];
@@ -27,17 +38,21 @@ void QT::initialize()
     {
         temp[i] = new QT[2];
         memset(temp[i], 0, sizeof(QT)*2);
+        //memset is memory set by 0 for temp[i]
+        //and It need string.h header
     }
     
     for(int i = 0; i<2; i++)
         for(int j = 0; j<2; j++)
             sub[i][j] = &temp[i][j];
     
-    
     for(int i = 0; i<2; i++)
         for(int j = 0; j<2; j++)
             sub[i][j]->value = 0;
 }
+
+//function void QT::print_value()
+//describe : print own value and sub's value by processed order
 void QT::print_value()
 {
     cout << value;
@@ -56,23 +71,31 @@ void QT::print_value()
     }
 }
 
+//declaration due to recursive call
 string input_data(QT* root, string input);
 
+//function string input_data(QT* root, string input)
+//describe : input first string data at own value
+//           if data is not x, return rest string
+//           else recursive call by each subtree, rest string
+//
+//return : rest string except own value
+//
+//parameter root - root of subtree
+//          input - thr rest input string
+//
 string input_data(QT* root, string input)
 {
     root->value = input[0];
     
     if (root->value != 'x')
-    {
-        //printf("%c",root->value);
         return input.erase(0,1);
-    }
     else
     {
         string ret;
         
-        root->initialize();
-        
+        root->initialize();     //이거 버그인 듯 한데 왜 성공했지?
+                                //root->value가 0으로 초기화되지않나?
         input = input_data(root->sub[0][0], input.erase(0,1));
         input = input_data(root->sub[0][1], input);
         input = input_data(root->sub[1][0], input);
@@ -82,6 +105,9 @@ string input_data(QT* root, string input)
     }
 }
 
+//int main(int argc, const char * argv[])
+//
+//attribute C - test case
 int main(int argc, const char * argv[]) {
     
     int C = 0;
@@ -93,9 +119,9 @@ int main(int argc, const char * argv[]) {
     {
         QT root;
         std::cin >> input;
-        input_data(&root, input);
+        input_data(&root, input);       //input
         
-        root.print_value();
+        root.print_value();             //output
         cout << endl;
     }
 }
