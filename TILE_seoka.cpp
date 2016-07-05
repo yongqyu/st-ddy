3d638f6c0c84e7d1a8372ab99515844cbfd8f735
seokaa <seoka_@naver.com>
2016-07-05 15:41:50 +0900

    tile

---
+++ TILE_seoka.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<stdlib.h>
+#include<cstring>
+#pragma warning(disable:4996)
+using namespace std;
+
+int T[512][512];
+
+void tile(int size, int startx, int starty, int holex, int holey) {
+	// 배수구 사분면 판별
+	int position = 1;
+	int newx = startx, newy = starty;
+	if (holex >= startx + size / 2) { position += 1; newx = startx + size / 2; }
+	if (holey >= starty + size / 2) { position += 2; newy = starty + size / 2; }
+
+	if (position != 1) T[starty + size / 2 - 1][startx + size / 2 - 1] = position;
+	if (position != 2) T[starty + size / 2 - 1][startx + size / 2] = position;
+	if (position != 3) T[starty + size / 2][startx + size / 2 - 1] = position;
+	if (position != 4) T[starty + size / 2][startx + size / 2] = position;
+
+	if (size != 2) {
+		if (position != 1) tile(size / 2, startx, starty, startx + size / 2 - 1, starty + size / 2 - 1);
+		if (position != 2) tile(size / 2, startx + size / 2, starty, startx + size / 2, starty + size / 2 - 1);
+		if (position != 3) tile(size / 2, startx, starty + size / 2, startx + size / 2 - 1, starty + size / 2);
+		if (position != 4) tile(size / 2, startx + size / 2, starty + size / 2, startx + size / 2, starty + size / 2);
+
+		tile(size / 2, newx, newy, holex, holey);
+	}
+
+	return;
+}
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	int n, x, y;
+	cin >> n;
+
+	cin >> y;
+	cin >> x;
+
+	tile(n, 0, 0, x, y);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			cout << T[i][j] << " ";
+		cout << endl;
+	}
+
+	return 0;
+}
