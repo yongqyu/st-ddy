c865d5c2994ee3bab6fc78f4393c90b104553f5c
seokaa <seoka_@naver.com>
2016-07-05 16:15:28 +0900

    Update TILE_seoka.cpp

--- TILE_seoka.cpp
+++ TILE_seoka.cpp
@@ -7,17 +7,19 @@ using namespace std;
 int T[512][512];
 
 void tile(int size, int startx, int starty, int holex, int holey) {
-	// 배수구 사분면 판별
+	// find hole position
 	int position = 1;
 	int newx = startx, newy = starty;
 	if (holex >= startx + size / 2) { position += 1; newx = startx + size / 2; }
 	if (holey >= starty + size / 2) { position += 2; newy = starty + size / 2; }
 
+	// fill position number
 	if (position != 1) T[starty + size / 2 - 1][startx + size / 2 - 1] = position;
 	if (position != 2) T[starty + size / 2 - 1][startx + size / 2] = position;
 	if (position != 3) T[starty + size / 2][startx + size / 2 - 1] = position;
 	if (position != 4) T[starty + size / 2][startx + size / 2] = position;
 
+	// half-size recursive
 	if (size != 2) {
 		if (position != 1) tile(size / 2, startx, starty, startx + size / 2 - 1, starty + size / 2 - 1);
 		if (position != 2) tile(size / 2, startx + size / 2, starty, startx + size / 2, starty + size / 2 - 1);
