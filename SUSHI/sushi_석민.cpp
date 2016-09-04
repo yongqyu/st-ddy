b599e7f9e78025ca1076ab9920a5555d44989da9
SeokMin Jang <mateo41@naver.com>
2016-09-04 18:11:32 +0900

    Add files via upload

---
+++ SUSHI/sushi_석민.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int c;
+
+	cin >> c;
+
+	for (int count = 0; count < c; count++) {
+		int n, m;
+		int price[20];
+		int pref[20];
+		int arr[10000000] = { 0, };
+
+		cin >> n >> m;
+
+		for (int i = 0; i < n; i++) {
+			cin >> price[i] >> pref[i];
+			arr[price[i] / 100] = pref[i];
+		}
+
+		for (int i = 1; i <= m/100; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i - (price[j] / 100) > 0) {
+					if (arr[i] < arr[i - (price[j] / 100)] + pref[j])
+						arr[i] = arr[i - (price[j] / 100)] + pref[j];
+				}
+			}
+		}
+
+		cout << arr[m/100] << endl;
+	}
+
+	system("pause");
+
+	return 0;
+}
